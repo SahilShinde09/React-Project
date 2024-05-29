b22390dbbec54276ffcf6fcbeaf2b9da426fda6f
SahilShinde09 <sahilshinde137@gmail.com>
2024-05-30 00:34:10 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -37,21 +37,21 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       {/* <Navbar title="Company" about="About us" /> */}
       {/* <Navbar /> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>      
       <div className="container my-3">
-      <Routes>
+      {/* <Routes>
         <Route exact path="/about" element={<About/>}>
         </Route>
         <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter your Text below" mode={mode}/>}>
         </Route>
-      </Routes>
+      </Routes> */}
+      <TextForm showAlert={showAlert} heading="Enter your Text below" mode={mode}/>
       </div>
-      {/* <TextForm showAlert={showAlert} heading="Enter your Text below" mode={mode}/> */}
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
